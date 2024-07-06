98e44bd4708da61400b0a01263683878a119c5a4
Rharhuky <rharhuky@gmail.com>
2024-07-06 07:53:49 -0300

    add: properties

--- src/main/java/com/rharhuky/spotify_api/SpotifyApiApplication.java
+++ src/main/java/com/rharhuky/spotify_api/SpotifyApiApplication.java
@@ -2,10 +2,12 @@ package com.rharhuky.spotify_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableConfigurationProperties
 public class SpotifyApiApplication {
 
 	public static void main(String[] args) {
