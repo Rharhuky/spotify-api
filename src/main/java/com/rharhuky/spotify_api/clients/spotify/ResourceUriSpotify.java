c5cf3158faada1c737d3c54ad7040fbdbaa265bb
Rharhuky <rharhuky@gmail.com>
2024-07-06 07:58:24 -0300

    add: ResourceUri

---
+++ src/main/java/com/rharhuky/spotify_api/clients/spotify/ResourceUriSpotify.java
@@ -0,0 +1,12 @@
+package com.rharhuky.spotify_api.clients.spotify;
+
+public class ResourceUriSpotify {
+
+    public static final String BASE_URI_AUTH = "https://accounts.spotify.com";
+
+    public static final String LOGIN_URI = "/api/token";
+
+    public static final String BASE_URI = "https://api.spotify.com";
+
+    public static final String NEW_RELEASE_ALBUM_URI = "/v1/browse/new-releases";
+}
---
+++ src/main/java/com/rharhuky/spotify_api/util/ResourceURI.java
@@ -0,0 +1,6 @@
+package com.rharhuky.spotify_api.util;
+
+public class ResourceURI {
+
+    public static final String BASE_URI = "/spotify/api";
+}
